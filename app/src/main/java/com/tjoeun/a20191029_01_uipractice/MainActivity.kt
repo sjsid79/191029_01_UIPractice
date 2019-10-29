9a6a7fe1a104da60b93df3327262ca821471741c
sjsid79 <wjdtlr0723>
2019-10-29 19:44:05 +0900

    최초 커밋

---
+++ app/src/main/java/com/tjoeun/a20191029_01_uipractice/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.tjoeun.a20191029_01_uipractice
+ // 20191029_01_UIPractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
